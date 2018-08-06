09a2fbeb4a2ff338eceef4aff047589af011e84b
Cate Palmer <catherinepalmer@gmail.com>
2018-08-06 12:08:22 +1200

    Got three of four tests passing for duplicate encoder

--- duplicate-encoder.js
+++ duplicate-encoder.js
@@ -1,35 +1,53 @@
-/* The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+// The goal of this exercise is to convert a string to a new string where each character 
+// in the new string is '(' if that character appears only once in the original string, or 
+// ')' if that character appears more than once in the original string. Ignore capitalization 
+// when determining if a character is a duplicate.
 
-Examples:
+// Examples:
 
-"din" => "((("
-
-"recede" => "()()()"
-
-"Success" => ")())())"
-
-"(( @" => "))((" */
+// "din" => "((("
+// "recede" => "()()()"
+// "Success" => ")())())"
+// "(( @" => "))(("
 
 
-function duplicateEncode(word){
-    const strArr = word.split('')
-    const sortedArr = strArr.sort()
-    let uniqueChars = []
-    let arrOfBrackets = []
-    sortedArr.forEach((char, i) => {
-      if (char[i] != char[i-1] && char[i] != char[i+1]) {
-        uniqueChars.push(char[i])
-        }
-      strArr.map(char => {
-        if (uniqueChars.find(uniqueChar => {
-          char == uniqueChar
-          })) {
-            arrOfBrackets.push('(')
-          }
-        else {
-          arrOfBrackets.push(')')
-        }
-      })
+function duplicateEncode(word) {
+  const strArr = word.split('')
+  const sortedArr = word.split('').sort()
+  const uniqueChars = getUniqueChars(sortedArr)
+  const bracketArr = replaceWithBrackets(strArr, uniqueChars)
+  return bracketArr.toString().replace(/,/g, "");
+}
+
+
+function getUniqueChars(sortedArr) {
+  let uniqueChars = []
+  if (sortedArr[0] != sortedArr[1]) {
+    uniqueChars.push(sortedArr[0])
+  }
+  for (let i=1; i < sortedArr.length-1; i++) {
+    if (sortedArr[i] != sortedArr[i-1] && sortedArr[i] != sortedArr[i+1]) {
+      uniqueChars.push(sortedArr[i])
+    }
+  }
+  if (sortedArr[sortedArr.length-1] != sortedArr[sortedArr.length-2]) {
+    uniqueChars.push(sortedArr[sortedArr.length-1])
+  }
+  return uniqueChars
+}
+
+
+function replaceWithBrackets(strArr, uniqueChars) {
+  let bracketArr = []
+  for (let i=0; i < strArr.length; i++) {
+    let matchedChars = uniqueChars.filter(char => {
+      return char == strArr[i]
     })
-  return arrOfBrackets.toString()
+    if (matchedChars.length > 0) {
+      bracketArr.push('(')
+    } else {
+      bracketArr.push(')')
+    }
+  }
+  return bracketArr
 }
