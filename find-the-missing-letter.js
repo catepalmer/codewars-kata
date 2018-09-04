29cf60a4d636eb1fd5b7c01e02ffe09579ab191d
Cate Palmer <catherinepalmer@gmail.com>
2018-09-04 17:01:08 +1200

    Got basic functionality of find-the-missing-letter working

--- find-the-missing-letter.js
+++ find-the-missing-letter.js
@@ -1,16 +1,72 @@
 function findMissingLetter(arr) {
 	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-	const alphabetIndex = alphabet.indexOf(arr[0].toLowerCase())
-	const letterBeforeMissing = arr.find((letter, i) => {
-	})
+	const indices = arr.map(letter => alphabet.indexOf(letter.toLowerCase()))
+	const missingLetterIndex = indices.find((num, i) => {
+		if (num != i) {
+			return i
+		}
+	}) - 1
+	if (arr[0].toLowerCase() === arr[0]) {
+		return alphabet[missingLetterIndex]
+	} else {
+		return alphabet[missingLetterIndex].toUpperCase()
+	}
 }
+
+
+
+
+
+
+
+// function findMissingLetter(arr) {
+// 	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+// 	const indices = arr.map(letter => alphabet.indexOf(letter.toLowerCase()))
+// 	const indexOfMissingLetter = indices.find((num, i) => {
+// 		if (num != i) {
+// 			return i
+// 		}
+// 	}) - 1
+// 	return alphabet[indexOfMissingLetter]
+// }
+
+
+
 
 // Pseudocode:
 
 // I need to check the alphabetIndex of each letter in arr, and if arr[i+1] 
 // has an alphabetIndex of 2 greater than arr[i], the letter with the alphabetIndex 
 // between arr[i] and arr[i+1] is the missing letter
+// Variables I need: alphabetIndex of each letter in arr
 
+
+
+
+// function findMissingLetter(arr) {
+// 	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+// 	const alphabetIndices = arr.map(letter => alphabet.indexOf(letter.toLowerCase()))
+// 	const indexBeforeMissing = alphabetIndices.find((number, i) => {
+// 		if (number[i] === number[i+1] + 2) {
+// 			return number[i]
+// 		}
+// 	})
+// 	console.log(indexBeforeMissing)
+
+// }
+
+
+
+
+
+
+// function findMissingLetter(arr) {
+// 	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+// 	for (let i = 0; i < alphabet.length; i++) {
+// 		let letterBeforeMissing = arr.find((letter, index) => {
+			
+// 		})
+// 	}
 
 
 
