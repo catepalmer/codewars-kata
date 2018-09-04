c78cc3a446adca836957e78c9b7f3f9b3bbe13ef
Cate Palmer <catherinepalmer@gmail.com>
2018-09-04 15:29:47 +1200

    Got basic functionality of countSmileys function working

--- count-the-smiley-faces.js
+++ count-the-smiley-faces.js
@@ -1,12 +1,54 @@
 function countSmileys(arr) {
-	arr.reduce((tally, str) => {
-		if (str === ':)' || str === ':D' || str === ':-)' || str === ':~)' || str === ':-D' || str === ':~D' || str === ';)' || str === ';D' || str === ';-)' || str === ';-D' || str === ';~)' || str === ';~D') {
-			tally++
-			return tally
+	const validSmileys = [':)', ':D', ':-)', ':~)', ':-D', ':~D', ';)', ';D', ';-)', ';~)', ';-D', ';~D']
+	return arr.reduce((count, str) => {
+		if (validSmileys.includes(str)) {
+			count++
 		}
-	} , 0)
+		return count
+	}, 0)
 }
 
+
+
+// function countSmileys(arr) {
+// 	const validSmileys = [':)', ':D', ':-)', ':~)', ':-D', ':~D', ';)', ';D', ';-)', ';~)', ';-D', ';~D']
+// 	arr.reduce((tally, str) => {
+// 		if (str === validSmileys.find(smiley => {
+// 			return smiley
+// 		})) {
+// 				tally++
+// 		}
+// 		return tally
+// 	}, 0)
+// }
+
+
+
+// function countSmileys(arr) {
+// 	const validSmileys = [':)', ':D', ':-)', ':~)', ':-D', ':~D', ';)', ';D', ';-)', ';~)', ';-D', ';~D']
+// 	arr.reduce((tally, str) => {
+// 		if (validSmileys.find(str => {
+// 			tally++
+// 			return tally
+// 		} else {
+// 			return tally
+// 		}))
+// 	}, 0)
+// }
+
+
+
+// function countSmileys(arr) {
+// 	arr.reduce((tally, str) => {
+// 		console.log(tally, str)
+// 		if (str === ':)' || str === ':D' || str === ':-)' || str === ':~)' || str === ':-D' || str === ':~D' || str === ';)' || str === ';D' || str === ';-)' || str === ';-D' || str === ';~)' || str === ';~D') {
+// 			tally++
+// 			return tally
+// 		} else {
+// 			return tally
+// 		}
+// 	} , 0)
+// }
 
 // Possible valid smileys:
 // :) :D :-) :~) :-D :~D ;) ;D ;-) ;~) ;-D ;~D
