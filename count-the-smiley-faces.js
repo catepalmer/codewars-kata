00c4207fba8432a0bfda3a04448534f0bd675e68
Cate Palmer <catherinepalmer@gmail.com>
2018-09-04 15:42:42 +1200

    Added best practices solution to comments underneath my countSmileys function after submitting final to codewars

--- count-the-smiley-faces.js
+++ count-the-smiley-faces.js
@@ -9,12 +9,8 @@ function countSmileys(arr) {
 
 
 
+// Best practices solution:
+
 // function countSmileys(arr) {
-// 	const validSmileys = [':)', ':D', ':-)', ':~)', ':-D', ':~D', ';)', ';D', ';-)', ';~)', ';-D', ';~D']
-// 	return arr.reduce((count, str) => {
-// 		if (validSmileys.includes(str)) {
-// 			count++
-// 		}
-// 		return count
-// 	}, 0)
+//   return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
 // }
