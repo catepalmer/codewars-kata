e26a6e7e088deb363410c97b6abace0c40ffe200
Cate Palmer <catherinepalmer@gmail.com>
2018-09-06 16:34:55 +1200

    Did some work on rgb function

--- rgb-to-hex-conversion.js
+++ rgb-to-hex-conversion.js
@@ -1,9 +1,61 @@
 function rgb(r, g, b){
-    let rHex = Math.floor(r/16) 
+  const hexes = '0123456789ABCDEF'.split('')
+  let rHex1 = hexes[Math.floor(r/16)]
+  let rHex2 = hexes[r/16]-hexes[Math.floor(r/16)]
+  console.log(rHex1, rHex2)
   }
 
 
 
+
+
+  So need to figure this out with some pseudocode:
+  const r1 = Math.floor(r/16)
+  if (r1 > 9) {
+
+  }
+
+
+
+//   function rgb(r, g, b){
+//     let rHex1 = ''
+//     let rHex2 = ''
+//     let gHex1 = ''
+//     let gHex2 = ''
+//     let bHex1 = ''
+//     let bHex2 = ''
+
+//     for (let i = 0; i < 256; i++) {
+
+//     }
+//   }
+
+//   const hexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+//   const r1 = Math.floor(r/16)
+//   if (r1 > 9) {
+      
+//   }
+
+
+//   const r1 = Math.floor((r+1)/16)
+//   let rHex1 = r1.forEach(num => {
+      
+//   })
+
+
+
+//   function rgb(r, g, b){
+//     let rHex1 = ''
+//     let rHex2 = ''
+//     let gHex1 = ''
+//     let gHex2 = ''
+//     let bHex1 = ''
+//     let bHex2 = ''
+
+//     for (let i = 0; i < 256; i++) {
+        
+//     }
+//   }
 
 
 
