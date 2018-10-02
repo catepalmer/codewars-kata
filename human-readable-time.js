ce2ea20b64a02883d6480accd655ac842bd53b48
Cate Palmer <catherinepalmer@gmail.com>
2018-10-02 20:09:57 +1300

    Tried to make human-readable function work for years and days and it's not there yet and also very long and unwieldy

--- human-readable-time.js
+++ human-readable-time.js
@@ -1,25 +1,29 @@
 function formatDuration(seconds) {
     let string = ''
+    const years = Math.floor(seconds / 31557600)
+    const days = Math.floor(seconds / 86400)
     const hours = Math.floor(seconds / 3600)
     const mins = Math.floor(seconds / 60 - hours * 60)
     const secs = seconds - hours * 3600 - mins * 60
-    if (hours > 0) {
-        string += `${hours.toString()} hour`
-        if (hours > 1) string += `s`
-        if (mins > 0 && secs > 0) string += `, `
-        if (mins > 0 && secs == 0) string += ` and `
-        if (mins == 0 && secs > 0) string += ` and `
+    const numArr = [years, days, hours, mins, secs]
+    const strArr = ['year', 'day', 'hour', 'minute', 'second']
+    let count = 0
+    let newArr = numArr.map(val => {
+        let tempStr = (val > 0) ? `${val.toString()} ${strArr[count]}` : ``
+        if (val > 1) tempStr += `s`
+        count++
+        return tempStr
+    })
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i] == '') {
+            newArr.splice(i, 1)
+        }
     }
-    if (mins > 0) {
-        string += `${mins.toString()} minute`
-        if (mins > 1) string += `s`
-        if (secs > 0) string += ` and `
-    }
-    if (secs > 0) {
-        string += `${secs.toString()} second`
-        if (secs > 1) string += `s`
-    }
-    return string
+    return newArr.map((val, i) => {
+        if (i < newArr.length -2) return `${val}, `
+        else if (i < newArr.length -1) return `${val} and `
+        else return val
+    }).join('')
   }
 
 
@@ -57,10 +61,17 @@ function formatDuration(seconds) {
 // You can find some examples in the test fixtures.
 
 
-
-// Test Results:
-// Expected: '1 second', instead got: '0:0:1'
-// Expected: '1 minute and 2 seconds', instead got: '0:1:2'
-// Expected: '2 minutes', instead got: '0:2:0'
-// Expected: '1 hour', instead got: '1:0:0'
-// Expected: '1 hour, 1 minute and 2 seconds', instead got: '1:1:2'
+// Expected: '182 days, 1 hour, 44 minutes and 40 seconds', instead 
+// got: '4369 hours, 44 minutes and 40 seconds'
+// Expected: '4 years, 68 days, 3 hours and 4 minutes', instead got: 
+// '36675 hours and 4 minutes'
+// Expected: '6 years, 192 days, 13 hours, 3 minutes and 54 seconds', 
+// instead got: '57181 hours, 3 minutes and 54 seconds'
+// Expected: '8 years, 12 days, 13 hours, 41 minutes and 1 second', 
+// instead got: '70381 hours, 41 minutes and 1 second'
+// Expected: '7 years, 246 days, 15 hours, 32 minutes and 54 seconds', 
+// instead got: '67239 hours, 32 minutes and 54 seconds'
+// Expected: '3 years, 85 days, 1 hour, 9 minutes and 26 seconds', 
+// instead got: '28321 hours, 9 minutes and 26 seconds'
+// Expected: '1 year, 19 days, 18 hours, 19 minutes and 46 seconds', 
+// instead got: '9234 hours, 19 minutes and 46 seconds'
