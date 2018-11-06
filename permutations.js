962c82949226293805ab77f934560fe98e72aca0
Cate Palmer <catherinepalmer@gmail.com>
2018-11-07 12:13:48 +1300

    Got basic functionality of permutations functions working in a recursive function

--- permutations.js
+++ permutations.js
@@ -1,85 +1,35 @@
-import { length, slice } from 'ramda'
-
-function factorial (str, fac = 1) {
-    if (length(str) === 1) return fac
-    fac *= length(str)
-    slice(0, length(str)-1, str)
-    return factorial(fac, str)
+function getPermsLength (str, count = 1, len = length(str)) {
+  if (len === 1) return count
+  count *= len
+  len--
+  return getPermsLength(str, count, len)
 }
 
-function permutations(string) {
-  let newStr = ''
-  let arr = []
+function permutations (str, perms = [], newStr = str, perm = '', idx = 0) {
+  const permsLength = getPermsLength(str)
 
-  newStr += string[0]
-  newStr += string[1]
-  newStr += string[2]
-  newStr += string[3]
+  if (length(perms) === permsLength) return perms
 
-  arr.push(newStr)
-
-  newStr = ''
-
-  newStr += string[0]
-  newStr += string[1]
-  newStr += string[3]
-  newStr += string[2]
-
-  arr.push(newStr)
-
-  newStr = ''
-}
-
-
-function permutations(arr = [], string) {
-
-
-    let newStr = ''
-
-    for (let i = 0; i < string.length; i++) {
-        newStr += string[i]
-    }
-    
-    arr.push(newStr)
-    count++
-  
-    newStr = ''
-  
-    newStr += string[0]
-    newStr += string[1]
-    newStr += string[3]
-    newStr += string[2]
-  
-    arr.push(newStr)
-  
-    newStr = ''
+  if (length(perm) === length(str)) {
+    append(perm, perms)
+    perm = ''
+    newStr = str
   }
 
-// permutations('cate'):
-
-// ['acet', 'acte', 'aect', 'aetc', 'atce', 'atec',
-// 'caet', 'cate', 'ceat', 'ceta', 'ctae', 'ctea',
-// 'eact', 'eatc', 'ecat', 'ecta', 'etac', 'etca',
-// 'tace', 'taec', 'tcae', 'tcea', 'teac', 'teca']
-
-
-// Want a temporary result (a new string) that
-// contains a permutation as it's being built. Then when
-// the length of this new string === the length of the
-// string that's passed in as an argument, the new string
-// is pushed to the array of permutations (that will be returned
-// at the end of the function once all the permutations
-// have been pushed to it).
+  perm += newStr[idx]
+  newStr = concat(slice(idx + 1, length(newStr), newStr), slice(0, idx, newStr))
+  idx++
+  
+  return (str, perms, newStr, perm, idx)
+}
 
 
+// // In this kata you have to create all permutations of an input string 
+// // and remove duplicates, if present. This means, you have to shuffle 
+// // all letters from the input in all possible orders.
 
+// // Examples:
 
-// In this kata you have to create all permutations of an input string 
-// and remove duplicates, if present. This means, you have to shuffle 
-// all letters from the input in all possible orders.
-
-// Examples:
-
-// permutations('a'); // ['a']
-// permutations('ab'); // ['ab', 'ba']
-// permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba'
+// // permutations('a'); // ['a']
+// // permutations('ab'); // ['ab', 'ba']
+// // permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba'
