3bbd963238cfd1902cf1e41a580e5eba51f109d1
Cate Palmer <catherinepalmer@gmail.com>
2018-11-08 13:27:46 +1300

    Wrote clunky recursive version of function using lots of conditionals

--- permutations.js
+++ permutations.js
@@ -1,23 +1,40 @@
-function getPermsLength (str, count = 1, len = length(str)) {
-  if (len === 1) return count
-  count *= len
-  len--
-  return getPermsLength(str, count, len)
+function getPermsLength (num, count = 1) {
+  if (num === 1) return count
+  count *= num
+  num--
+  return getPermsLength(num, count)
 }
 
-function permutations (string) {
-  const permsLength = getPermsLength(string)
-  let perms = []
-  let perm = string
 
-  for (let i = 0; i < permsLength; i++) {
-    perms.push(perm)
-    perm = concat(perm[length(perm) - 3], concat(slice(0, length(perm) - 3, perm), slice(length(perm) - 2, length(perm), perm)))
+function permutations(str, perms = [], idx = 0) {
+  const permsLen = getPermsLength(strLen)
+  const strLen = length(str)
+
+  if (idx === permsLen) return perms
+
+  if (idx === 0) {
+    str = str
   }
-  return perms
+  else if (idx % (permsLen / strLen) === 0) {
+    if ((permsLen - idx) === (permsLen / strLen - 1)) {
+      str = str[strLen-1] + slice(1, strLen-1, str) + str[0] 
+    }
+    else {
+      str = str[1] + str[0] + slice(2, strLen, str)
+    }
+  }
+  else if (idx % ((permsLen / strLen) / (strLen-1)) === 0) {
+    str = str[0] + str[2] + str[1] + str[strLen-1]
+  }
+  else {
+    str = slice(0, strLen-2, str) + str[strLen-1], str[strLen-2]
+  }
+  perms.push(str)
+  idx++
+
+  return permutations(str, perms, idx)
 }
 
-['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
 
 
 
@@ -25,14 +42,155 @@ function permutations (string) {
 
 
 
-function getPermsLength (str, count = 1, len = length(str)) {
+
+// for (let h = 0; h < permsLength; h++) {
+//   if (h === 0) {
+//     perm = perm
+//   }
+//   else if (h % (permsLength / str.length) === 0) {
+//     if (this is the last one of this kind) {
+//       perm = switch perm[0] and perm[3], leave [1] and [2] the same
+//     }
+//     perm = switch perm[0] and perm[1], leave rest of string the same
+//   }
+//   else if (h % ((permsLength / str.length) / (str.length -1)) === 0) {
+//     perm = switch perm[1] and perm[2], leave rest of string the same
+//   }
+//   else {
+//   perm = switch perm[2] and perm[3], leave rest of string the same
+//   }
+//   perms.push(perm)
+// // Then pass the modified form of perm to the next call of the function
+// }
+
+
+
+// To change the output each time, need to also pass to the recursive function call
+// an iterator for the characters that are being swapped.
+
+
+
+
+// function getPermsLength (num, count = 1) {
+//   if (num === 1) return count
+//   count *= num
+//   num--
+//   return getPermsLength(num, count)
+// }
+
+
+// function permutations(str, perms = [], strLen = length(str)) {
+//   const permsLen = getPermsLength(strLen)
+//   let perm = ''
+
+//   if (length(perms) === permsLen) return perms
+
+//   for (let i = 0; i < strLen; i++) {
+//     perm += str[i]
+//   }
+//   perms.push(perm)
+//   return permutations(perm, perms, strLen)
+// }
+
+
+
+
+// function getPermsLength (num, count = 1) {
+//   if (num === 1) return count
+//   count *= num
+//   num--
+//   return getPermsLength(num, count)
+// }
+
+
+// function permutations(str, perms = [], perm = '', strLen = length(str)) {
+//   const permsLen = getPermsLength(strLen)
+
+//   if (length(perms) === permsLen) return perms
+
+//   for (let i = 0; i < strLen; i++) {
+//     perm += str[i]
+//   }
+//   perms.push(perm)
+//   perm = ''
+//   return permutations(str, perms, perm, strLen)
+// }
+
+
+// function permutations(str, perms = [], perm = '', strLen = length(str)) {
+//   while (true) {
+//     if(length(perm) === length(str)) perms.push(perm)
+//     strLen--
+
+//     if (strLen < 1) break
+//   }
+//   return perms
+// }
+
+
+
+
+// function permutations(string) {
+//   let perms = []
+//   let perm = string
+//   let string2 = string
+
+//   while (length(string2) > 0) {
+//     for (let i = 0; i < length(string2); i++) {
+//       perms.push(perm)
+//     }
+//     string2--
+//   }
+//   return perms
+// }
+
+
+
+
+
+
+// function getPermsLength (num, count = 1) {
+//   if (num === 1) return count
+//   count *= num
+//   num--
+//   return getPermsLength(num, count)
+// }
+
+// function permutations (string) {
+//   const permsLength = getPermsLength(length(string))
+//   let perms = []
+//   let perm = string
+//   let string2 = string
+
+//   for (let i = 0; i < length(string2); i++) {
+
+//     string2--
+//     for (let j = 0; j < length(string2) - 1; j++) {
+//       perms.push(perm)
+
+
+//       perm = concat(perm[length(perm) - 3], concat(slice(0, length(perm) - 3, perm), slice(length(perm) - 2, length(perm), perm)))
+//     }
+//   }
+//   return perms
+// }
+
+// ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+
+
+
+
+
+
+function getPermsLength(str, count = 1, len = length(str)) {
   if (len === 1) return count
   count *= len
   len--
   return getPermsLength(str, count, len)
 }
 
-function permutations (string) {
+function permutations(string) {
   const permsLength = getPermsLength(string)
   let perms = []
   let perm = string
@@ -40,12 +198,12 @@ function permutations (string) {
   for (let i = 0; i < permsLength; i++) {
     perms.push(perm)
     if (i < length(string)) {
-     perm = concat(slice(i + 1, length(string), perm), slice(0, i + 1, perm))
+      perm = concat(slice(i + 1, length(string), perm), slice(0, i + 1, perm))
     }
     else if (i < length(string) * 2) {
-     perm = concat(slice(i + 1, length(string), perm), slice(0, i + 1, perm))
+      perm = concat(slice(i + 1, length(string), perm), slice(0, i + 1, perm))
     }
-    
+
   }
   return perms
 }
@@ -74,8 +232,8 @@ function permutations (string) {
 11 = 'cate' // perm[0] switch with perm[3]
 12 = 'ctae' // perm[1] switch with perm[2]
 13 = 'ceat' // perm[1] switch with perm[3]
-14 = 
-15
+14 =
+  15
 16
 17
 18
@@ -100,7 +258,7 @@ function permutations (string) {
 //   perm += newStr[idx]
 //   newStr = concat(slice(idx + 1, length(newStr), newStr), slice(0, idx, newStr))
 //   idx++
-  
+
 //   return permutations (str, perms, newStr, perm, idx)
 // }
 
@@ -114,17 +272,17 @@ function permutations (string) {
 //   let perm = ''
 
 //   for (let i = 0; i < permsLength; i++) {
-    
+
 //     for (let j = 0; j < length(string); j++) {
 //       perm += newStr[j]
 //       newStr = concat(slice(j+1, length(newStr), newStr), slice(0, j, newStr))
-      
+
 //       if (length(perm) === length(string)) {
 //       perms.push(perm)
 //       perm = ''
 //       newStr = string
 //     }
-      
+
 //     }
 
 
@@ -167,7 +325,7 @@ function permutations (string) {
 //       perm += newStr[j]
 //     }
 //     newStr2 = 
-      
+
 //     }
 
 
@@ -186,7 +344,7 @@ function permutations (string) {
 //       newStr = string
 //       idx = 0
 //     }
-    
+
 //     perm += newStr[idx]
 //     newStr = concat(slice(idx + 1, length(newStr), newStr), slice(0, idx, newStr))
 //     idx++
@@ -210,7 +368,7 @@ function permutations (string) {
 //   perm += newStr[idx]
 //   newStr = concat(slice(idx + 1, length(newStr), newStr), slice(0, idx, newStr))
 //   idx++
-  
+
 //   return permutations (str, perms, newStr, perm, idx)
 // }
 
