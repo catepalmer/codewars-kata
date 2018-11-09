331576da93daa62bab08b9bec7a6304c99647210
Cate Palmer <catherinepalmer@gmail.com>
2018-11-09 18:56:38 +1300

    removeNb function now works

--- is-my-friend-cheating.js
+++ is-my-friend-cheating.js
@@ -1,17 +1,18 @@
-import { add, identity, reduce, reverse, times } from 'ramda'
-
-function sumNums (n, total = 0) {
-  while (n > 0) {
-    n = n - 1
-    total = total + n
-  }
-  return total
-}
+import { add, identity, isEmpty, length, not, reduce, remove, times } from 'ramda'
 
 function removeNb(n) {
-  return reduce(add, n, reverse(times(identity, n)))
+  const nums = remove(0, 1, times(identity, n + 1))
+  const sum = reduce(add, 0, nums)
+  let arr = []
+
+  for (let i = 0; i < length(nums); i++) {
+    for (let j = 0; j < length(nums); j++) {
+      let pairArr = []
+      if ((nums[i] * nums[j] === sum - nums[i] - nums[j]) && i !== j) {
+        pairArr.push(nums[i], nums[j])
+        if (not(isEmpty(pairArr))) arr.push(pairArr)
+      }
+    }
+  }
+  return arr
 }
-
-
-reduce(R.subtract, 0, [1, 2, 3, 4])
-R.times(R.identity, 5); //=> [0, 1, 2, 3, 4]
